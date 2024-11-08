3171029e48c19dae2ecfb34f6dfc7b283f524bd7
Islam Saimun <s2400191@BC-C02J61E6Q6LR-MacBook-Air.local>
2024-11-08 12:33:31 +0200

    add Loginscreen

---
+++ src/LoginScreen/LoginScreen.jsx
@@ -0,0 +1,28 @@
+
+
+const LoginScreen = () => {
+    return (
+      <div className="w-full h-full flex flex-col items-center justify-center relative">
+        <div className="w-full h-full absolute top-0 left-0">
+          
+        </div>
+        <div
+          id="login-page"
+          className="flex flex-col items-center absolute top-0 left-0">
+          <h2 className="text-5xl text-white font-bold tracking-wide capitalize text-center mt-20">
+            MusicApp
+          </h2>
+  
+          <div className="flex flex-col items-center justify-center mt-10">
+            <button
+              className="w-fit px-5 py-2 text-lg capitalize text-white bg-[#1ed760] rounded-full font-bold">
+              connect to Jampa
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default LoginScreen;
+  
