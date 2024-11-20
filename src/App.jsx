fd9280c4abb350a6845734923e13219699bc95e5
Pujakumari1511 <puja.kumari6494@gmail.com>
2024-11-20 04:50:31 +0200

    Remove unused data

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import './AuthTokenApi/AuthTokenApi.js'
 import { getAccessToken } from './AuthTokenApi/AuthTokenApi.js'  // used to get the token for API calls
 import './ApiFunctions/ApiFunctions.js'
 import { fetchProfile, getTrack, playMusic, getAvailableDevices, startPlayback, pausePlayback } from './ApiFunctions/ApiFunctions.js' // Some API calls to understand how the work
-import * as bootstrap from'bootstrap';
 import TestModal from './TestModal.jsx'
 
 
