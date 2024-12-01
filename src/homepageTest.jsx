14370587f03609dc629b516bf23b67c102183f7a
iamharryda <sayeedanwar@protonmail.com>
2024-12-01 14:04:21 +0200

    added homepage test, homepage css, and just changed the root from app css to avoid the margin

---
+++ src/homepageTest.jsx
@@ -0,0 +1,23 @@
+import '/Users/s2400057/Documents/my_studies/software developement/projects/JAMPA/src/Homepage.css'
+export const Homepage = () => {
+
+    return (
+        <div className='homepage'>
+            <header>
+                <h1>header</h1>
+            </header>
+            <main>
+                <div className='sidebar'>
+                    <h3>Sidebar</h3>
+                </div>
+                <div className='content'>
+                    <h3>Content</h3>
+                </div>
+            </main>
+            <footer>
+                <h3>copyright Jampa</h3>
+            </footer>
+            <div className='popup'><h3>popup player</h3></div>
+        </div>
+    )
+}
